3fb591d7b2203eb468fd6401428da367094e496d
jaovy05 <joaovitorg.silva050@gmail.com>
2023-11-15 16:37:01 -0300

    Adicionado comentários

--- src/Carta.java
+++ src/Carta.java
@@ -6,26 +6,38 @@ public class Carta {
     private Integer pontos;
 
     public Carta(int numero){
+        //ponto base
         pontos = 1;
+        //se terminar com 5, recebe um ponto
         if(numero % 10 == 5) pontos ++;
+        //se for divisível por 10, ganha dois pontos
         else if(numero % 10 == 0) pontos += 2;
+        //se tiver número repetido, ganha quatro pontos
         numeroRepetido(numero);
 
         this.numero = numero;
     }
 
     private void numeroRepetido(int numero){
+        //utiliza conjunto, pois conjuntos não aceitam elementos duplicados
         Set<Integer> conjuntoNumeros = new HashSet<>();
-        //Se não for possivel adicionar o int, é porque tem um elemento repetido no conjunto
+
+        //o for vai repetir até separar todos os digitos
         for(int i = 1; i <= numero; i *= 10)
-            if(!conjuntoNumeros.add( (numero / i) % 10 )) pontos += 4;
+            //se não for possivel adicionar, entra no if somando pontos
+            if(!conjuntoNumeros.add( (numero / i) % 10 )){
+                pontos += 4;
+                break;
+            }
     }
 
     @Override
+    //sobrescrita do método toString para facilitar o print
     public String toString(){
         return numero + " ";
     }
 
+    //daqui para baixo contém somente os Getters e Setters
     public Integer getPontos() {
         return pontos;
     }
