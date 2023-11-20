ba40f9e548ae76779c43b87c20390a03ca5fe8dc
jaovy05 <joaovitorg.silva050@gmail.com>
2023-11-19 23:08:56 -0300

    adicionado comentários

--- src/classes/Tabuleiro.java
+++ src/classes/Tabuleiro.java
@@ -22,11 +22,13 @@ public class Tabuleiro {
         for(int i = 0; i < 5; i++) {
             try{
                 tabuleiro.add(new LinkedList<>());
+                //adiciona a primeira carta das linhas
                 tabuleiro.get(i).addLast(baralho.comprar());
             } catch(Exception e){
                 System.out.println(e.getMessage());
             }
         }
+        //mostra a situação inicial do tabuleiro
         printTabuleiro();
         sc.close();
     }
@@ -40,29 +42,42 @@ public class Tabuleiro {
     }
 
     public void rodada(){
+        //pega a carta de todos jogadores
         for(Jogador j : jogadores){
             boolean naoEscolheu = true;
+            //enquanto não escolher o loop continua
             while (naoEscolheu){
                 try {
+                    //adiciona a carta jogada ao jogador
                     j.setCartaJogada((escolherCarta(j)));
                     naoEscolheu = false;
                 } catch(Exception e){
+                    //diz para escolher a carta certo
                     System.out.println(e.getMessage());
                 }
             }
         } 
+        //ordena os jogodares ordem crescente
         Collections.sort(jogadores, new Sort());
         for(Jogador j : jogadores){
+            //pega o índice do antecessor mais próxima
             Integer indexAntecessor = antecessor(j.getCartaJogada());
-            if(indexAntecessor == -1)
-              j.comprarLinha(tabuleiro.get(posMaiorElemento()));
-            else {
+            if(indexAntecessor == -1){
+                //se não tiver antecessor, compra a linha com maior elemento
+                j.comprarLinha(tabuleiro.get(posMaiorElemento()));
+                //adiciona a carta na linha em questão
+                tabuleiro.get(posMaiorElemento()).addLast(j.getCartaJogada());
+            } else {
+                //se a linha tiver cheia, compra a mesma
                 if(tabuleiro.get(indexAntecessor).size() ==  5)
                     j.comprarLinha(tabuleiro.get(indexAntecessor));
+                //adiciona a carta na linha do antecessor
                 tabuleiro.get(indexAntecessor).addLast(j.getCartaJogada());
             }
         }
+        //mostra a situação do tabuleiro
         printTabuleiro();
+        //e dos jogadores
         printJogadores();
     }
 
@@ -76,43 +91,51 @@ public class Tabuleiro {
 
     public Carta escolherCarta(Jogador jogador) throws Exception{
         Scanner sc = new Scanner(System.in);
+        //chama o jogador pelo nome
         System.out.println("Escolha sua carta " + jogador.getNome() + ":");
-        for (int i = 0; i < 5; i++) {
-            for(Carta c : jogador.getMaoJogador()) System.out.print(c);
-            System.out.println();
-        }
+        //printa a mão do jogador
+        for(Carta c : jogador.getMaoJogador()) System.out.print(c);
         Integer numero = sc.nextInt();
         sc.close();
+
         for(Carta c : jogador.getMaoJogador()) 
+            //verifica se o número informado está na mão
             if(c.getNumero() == numero) {
+                //se tiver, remove da mão e retorna a carta
                 jogador.getMaoJogador().remove(c);
                 return c;
             }
-
+        //se não encontrar retorna exception
         throw new Exception("Escolha uma carta válida");
     }
 
     public Integer antecessor(Carta cartaJogada){
         Integer antecessor = 0, pos = -1, ultimaCartadaLinha;
         for(int i = 0; i < 5; i++){
+            //pega a última Carta da Linha
             ultimaCartadaLinha = tabuleiro.get(i).getLast().getNumero();
+            //se a última carta for menor que a carta jogada é maior que o antecessor, entra no if
             if(ultimaCartadaLinha < cartaJogada.getNumero() && ultimaCartadaLinha > antecessor){
+                    //atualiza o antecessor
                     antecessor = ultimaCartadaLinha;
+                    //pega a posição do antecessor
                     pos = i;
             }
         }
+        //retorna a pos
         return pos;
     }
 
     public Integer posMaiorElemento(){
         Integer pos = 0, maior = 0;
+
         for(int i = 0; i < 5; i++){
             if(maior < tabuleiro.get(i).getLast().getNumero()){
+                //atuliza o maior e a posição
                 maior = tabuleiro.get(i).getLast().getNumero();
                 pos = i;
             }
         }
         return pos;
-        
     }
 }
